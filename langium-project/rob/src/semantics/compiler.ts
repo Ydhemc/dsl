49347840d202273f1acab5161160e623496852a5
ollprogram <39884051+ollprogram@users.noreply.github.com>
2025-02-13 14:49:58 +0100

    started compile

--- langium-project/rob/src/semantics/compiler.ts
+++ langium-project/rob/src/semantics/compiler.ts
@@ -1,4 +1,4 @@
-import { RobotProgram } from "../semantics/robot-ml-visitor.js";
+import { ArithmeticExpr, Assignment, Backward, BinaryArithmetic, BinaryBool, Block, Bool, BooleanExpr, Call, CallExpr, Condition, Declaration, Expression, FalseExpr, Forward, Func, Instruction, Left, Loop, Movement, Negative, Not, NumeralExpr, Parameter, Real, Return, Right, RobotMLVisitor, RobotProgram, Rotate, Sensor, SensorDistance, SensorExpr, SensorTime, Speed, TrueExpr, Type, VarExpr, Variable } from "../semantics/robot-ml-visitor.js";
 import { expandToNode, toString } from 'langium/generate';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
@@ -43,4 +43,128 @@ export function generateArduino(model: RobotProgram, filePath: string, destinati
     }
     fs.writeFileSync(generatedFilePath, toString(fileNode));
     return generatedFilePath;
+}
+
+
+export class RobotVisitorImpl implements RobotMLVisitor {
+
+
+    visitRobotProgram(node: RobotProgram) {
+        throw new Error("Method not implemented.");
+    }
+
+    visitDeclaration(node: Declaration) {
+        throw new Error("Method not implemented.");
+    }
+
+    visitFunc(node: Func) {
+        throw new Error("Method not implemented.");
+    }
+
+    visitSensor(node: Sensor) {
+        throw new Error("Method not implemented.");
+    }
+    visitSensorDistance(node: SensorDistance) {
+        throw new Error("Method not implemented.");
+    }
+    visitSensorTime(node: SensorTime) {
+        throw new Error("Method not implemented.");
+    }
+    visitVariable(node: Variable) {
+        throw new Error("Method not implemented.");
+    }
+    visitParameter(node: Parameter) {
+        throw new Error("Method not implemented.");
+    }
+    visitExpression(node: Expression) {
+        throw new Error("Method not implemented.");
+    }
+    visitArithmeticExpr(node: ArithmeticExpr) {
+        throw new Error("Method not implemented.");
+    }
+    visitBinaryArithmetic(node: BinaryArithmetic) {
+        throw new Error("Method not implemented.");
+    }
+    visitNegative(node: Negative) {
+        throw new Error("Method not implemented.");
+    }
+    visitBinaryBool(node: BinaryBool) {
+        throw new Error("Method not implemented.");
+    }
+    visitBooleanExpr(node: BooleanExpr) {
+        throw new Error("Method not implemented.");
+    }
+    visitNot(node: Not) {
+        throw new Error("Method not implemented.");
+    }
+    visitFalseExpr(node: FalseExpr) {
+        throw new Error("Method not implemented.");
+    }
+    visitTrueExpr(node: TrueExpr) {
+        throw new Error("Method not implemented.");
+    }
+    visitCallExpr(node: CallExpr) {
+        throw new Error("Method not implemented.");
+    }
+    visitNumeralExpr(node: NumeralExpr) {
+        throw new Error("Method not implemented.");
+    }
+    visitSensorExpr(node: SensorExpr) {
+        throw new Error("Method not implemented.");
+    }
+    visitVarExpr(node: VarExpr) {
+        throw new Error("Method not implemented.");
+    }
+    visitInstruction(node: Instruction) {
+        throw new Error("Method not implemented.");
+    }
+    visitAssignment(node: Assignment) {
+        throw new Error("Method not implemented.");
+    }
+    visitBlock(node: Block) {
+        throw new Error("Method not implemented.");
+    }
+    visitCall(node: Call) {
+        throw new Error("Method not implemented.");
+    }
+    visitCondition(node: Condition) {
+        throw new Error("Method not implemented.");
+    }
+    visitLoop(node: Loop) {
+        throw new Error("Method not implemented.");
+    }
+    visitMovement(node: Movement) {
+        throw new Error("Method not implemented.");
+    }
+    visitBackward(node: Backward) {
+        throw new Error("Method not implemented.");
+    }
+    visitForward(node: Forward) {
+        throw new Error("Method not implemented.");
+    }
+    visitLeft(node: Left) {
+        throw new Error("Method not implemented.");
+    }
+    visitRight(node: Right) {
+        throw new Error("Method not implemented.");
+    }
+    visitRotate(node: Rotate) {
+        throw new Error("Method not implemented.");
+    }
+    visitReturn(node: Return) {
+        throw new Error("Method not implemented.");
+    }
+    visitSpeed(node: Speed) {
+        throw new Error("Method not implemented.");
+    }
+    visitType(node: Type) {
+        throw new Error("Method not implemented.");
+    }
+    visitBool(node: Bool) {
+        throw new Error("Method not implemented.");
+    }
+    visitReal(node: Real) {
+        throw new Error("Method not implemented.");
+    }
+    
 }

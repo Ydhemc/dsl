d89901b4705a6e56a5c4c221b0b87b4e51dd750b
mchater <mehdy.chater@etudiant.univ-rennes.fr>
2025-02-13 09:31:25 +0100

    ajout validation

--- langium-project/rob/src/language/robot-ml-validator.ts
+++ langium-project/rob/src/language/robot-ml-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { Func, RobotMLAstType } from './generated/ast.js';
+import { Forward, Func, Movement, RobotMLAstType, RobotProgram } from './generated/ast.js';
 import type { RobotMLServices } from './robot-ml-module.js';
 
 /**
@@ -9,7 +9,8 @@ export function registerValidationChecks(services: RobotMLServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.RobotMLValidator;
     const checks: ValidationChecks<RobotMLAstType> = {
-        Func: validator.checkFuncNameLowerCamelCase
+        Func: [validator.checkFuncNameLowerCamelCase, validator.checkUniqueParams, validator.checkUnusedParams],
+        RobotProgram: validator.checkUniqueFunc
     };
     registry.register(checks, validator);
 }
@@ -21,7 +22,6 @@ export function registerValidationChecks(services: RobotMLServices) {
  */
 export class RobotMLValidator {
 
-    
     checkFuncNameLowerCamelCase(func: Func, accept: ValidationAcceptor): void {
         const camelCaseRegex = /^[a-z]+(?:[A-Z0-9]+[a-z0-9]+[A-Za-z0-9]*)*$/
         if (!camelCaseRegex.test(func.name)) {
@@ -29,4 +29,37 @@ export class RobotMLValidator {
         }
     }
 
+    checkUniqueParams(func: Func, accept: ValidationAcceptor): void {
+        const reported = new Set();
+        func.parameter.forEach(p => {
+            if (reported.has(p.name)) {
+                accept('error', `Param ${p.name} is non-unique for Def '${func.name}'`, {node: p, property: 'name'});
+            }
+            reported.add(p.name);
+        });
+    }
+
+    checkUniqueFunc(Program: RobotProgram, accept: ValidationAcceptor): void {
+        const reported = new Set();  
+        
+        Program.declarations.forEach(d => {
+            if(d.$type==Func && reported.has((d as Func).name)) {
+                accept('error', `Param ${(d as Func).name} is non-unique for Def '${(d as Func).name}'`, {node: d, property: 'name'});
+            }
+            reported.add((d as Func).name);
+        });
+    }
+
+    checkUnusedParams(func: Func, accept: ValidationAcceptor): void {
+        func.parameter.forEach(param => {
+            /*
+            if (func.instruction.$type==Block ) { 
+                accept('warning', `Parameter '${param.name}' is declared but never used in function '${func.name}'.`, { node: param, property: 'name' });
+            }*/
+            if(func.instruction.$type==Forward && !(func.instruction as Movement).parameter.variable.includes(param)){
+                accept('warning', `Parameter '${param.name}' is declared but never used in function '${func.name}'.`, { node: param, property: 'name' });    
+            }
+        });
+    }
+    
 }
